ec3c139467f8aa5e2c244e8f0b1998ad0a5f4dde
leetmdgus <lee_tmdgus@naver.com>
2022-05-11 13:29:25 +0900

    web study 2022 05 11

---
+++ 3.momentum/1.console/app.js
@@ -0,0 +1,31 @@
+let a = 5;
+const b = "2";
+
+// let a = "5";
+var c = 4;
+var c = 5;
+
+const list = [1,b,a,"hello",c, null, undefined];
+
+console.log(list);
+list.push(3);
+
+
+
+const player ={
+  name:"Hyun",
+  points:10,
+  fat:true,
+  sayHello : function(otherPersonsName){
+    console.log("Hello! "+otherPersonsName + "nice to meet you! ");
+  }
+};
+
+player.lastName = "Lee";
+
+console.log(player);
+
+console.log(545621321)
+
+
+// ctrl + D: 독같은 텍스트 다중 선택
---
+++ 4.learn to JS/1. hello world/index.js
@@ -0,0 +1,21 @@
+console.log("Hello world"); 
+console.log("Hello world"); 
+console.log("Hello world"); 
+
+// 이것은 코멘트 입니다. 
+/*이것도 코멘트 입니다. 
+와웅 */
+
+let espressoPrice = 3000;
+let lattePrice = 4300;
+let mochaPrice = 4000;
+
+function func(sentense) {
+  console.log("Hello world");
+  console.log("Hi");
+  console.log(sentense)
+  return 3;
+}
+
+
+
